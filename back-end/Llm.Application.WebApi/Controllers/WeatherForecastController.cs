e86402ee318ae1f74089d5006ae259ca0aea7749
Abusaleh Md Nayeem <abusaleh.nayeem@hana-ltd.com>
2024-10-21 15:11:26 +0600

    initial check-in

---
+++ back-end/Llm.Application.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Llm.Application.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+    }
+}
